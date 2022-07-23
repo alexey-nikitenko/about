d4baa8b393725c2fed0ffa26f6b02a4510dcc6a9
Aleksey Nikitenko <donaspeedgithub@gmail.com>
2022-07-23 18:29:00 +0200

    Change background gradient and icon

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ function Header() {
             <div className="navbar navbar-dark bg-dark shadow-sm">
                 <div className="container">
                     <a href="#" className="navbar-brand d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" /><circle cx="12" cy="13" r="4" /></svg>
-                        <strong>Album</strong>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-robot" viewBox="0 0 20 20">
+  <path d="M6 12.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5ZM3 8.062C3 6.76 4.235 5.765 5.53 5.886a26.58 26.58 0 0 0 4.94 0C11.765 5.765 13 6.76 13 8.062v1.157a.933.933 0 0 1-.765.935c-.845.147-2.34.346-4.235.346-1.895 0-3.39-.2-4.235-.346A.933.933 0 0 1 3 9.219V8.062Zm4.542-.827a.25.25 0 0 0-.217.068l-.92.9a24.767 24.767 0 0 1-1.871-.183.25.25 0 0 0-.068.495c.55.076 1.232.149 2.02.193a.25.25 0 0 0 .189-.071l.754-.736.847 1.71a.25.25 0 0 0 .404.062l.932-.97a25.286 25.286 0 0 0 1.922-.188.25.25 0 0 0-.068-.495c-.538.074-1.207.145-1.98.189a.25.25 0 0 0-.166.076l-.754.785-.842-1.7a.25.25 0 0 0-.182-.135Z"/>
+  <path d="M8.5 1.866a1 1 0 1 0-1 0V3h-2A4.5 4.5 0 0 0 1 7.5V8a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v1a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1v-.5A4.5 4.5 0 0 0 10.5 3h-2V1.866ZM14 7.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.5A3.5 3.5 0 0 1 5.5 4h5A3.5 3.5 0 0 1 14 7.5Z"/>
+</svg>
+                        <strong> Aleksey Nikitenko projects</strong>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -38,7 +41,9 @@ function Footer() {
     return (
         <div className="container">
             <p className="float-end mb-1">
-                <a href="#">Back to top</a>
+                <a href="#">
+                <strong>Back to top</strong>
+                    </a>
             </p>
         </div>
     );
@@ -50,7 +55,9 @@ function Main() {
             <section className="py-5 text-center container">
                 <div className="row py-lg-5">
                     <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">Pages</h1>
+                        <h1 className="fw-light">
+                            <strong>Pages</strong>
+                            </h1>
                     </div>
                 </div>
             </section>
@@ -59,7 +66,7 @@ function Main() {
                 <div className="container">
                     <div className="row row-cols-1 row-cols-md-3 g-4">
                         <div class="col">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img src="./images/kikstarter_link_beatify.jpg" className="card-img-top" alt="..." />
 
                                 <div className="card-body">
@@ -70,7 +77,7 @@ function Main() {
                             </div>
                         </div>
                         <div class="col">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img src="./images/mouse_manipulator.jpg" className="card-img-top" alt="..." />
 
                                 <div className="card-body">
@@ -81,7 +88,7 @@ function Main() {
                             </div>
                         </div>
                         <div class="col">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img src="./images/kikstarter_link_beatify.jpg" className="card-img-top" alt="..." />
 
                                 <div className="card-body">
@@ -92,7 +99,7 @@ function Main() {
                             </div>
                         </div>
                         <div class="col">
-                            <div className="card">
+                            <div className="card shadow-sm">
                                 <img src="./images/mouse_manipulator.jpg" className="card-img-top" alt="..." />
 
                                 <div className="card-body">
