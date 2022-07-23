3f0356cd4a50d945a0546a512a964f2e4619aa7e
Aleksey Nikitenko <donaspeedgithub@gmail.com>
2022-07-23 13:49:58 +0200

    Use React

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    ReactDOM.render(<h1>
+        Hello.
+        <br />
+        Thank you for visiting this page.
+        <br />
+        It's under renovation.
+        But you can visit my kikstarter project page.
+        <p>
+            <a
+                href="https://www.kickstarter.com/projects/myroboarm/robotic-arms-as-manipulator-for-keyboards-and-mouse">
+                <img src="./images/kikstarter_link.png" alt="Project image"></img>
+            </a>
+        </p>
+    </h1>, document.getElementById('root'));
